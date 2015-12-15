318b208a143bb005dff59ba4a64531ab77c95d9c
xigolle <joey.driessen@live.nl>
2015-12-15 13:23:56 +0100

    Addes express server + return HTML when connected
    Addess express server and return HTML file when user goes to
    localhost:3000

---
+++ server.js
@@ -0,0 +1,18 @@
+﻿/* <summary>
+ * 
+ Node JS file for the server configuration 
+ Server works on port 3000
+ </summary>*/
+
+var express = require("express");
+var app = express();
+var path = require('path');
+
+
+
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname+"/index.html"));
+});
+
+app.listen(3000);

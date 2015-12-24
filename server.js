81e882ab8d908168196b4fe9225cf062b1aa91ee
xigolle <joey.driessen@live.nl>
2015-12-24 14:54:27 +0100

    Made sure all the files are being server by the server
    Alle js files and such will be server when put in the correct folder (
    root folder of the server ). You can make sub dirs if you want!

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var path = require('path');
 
 
 
-
+app.use(express.static(path.join(__dirname, '')));
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname+"/index.html"));
 });

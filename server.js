d6a56cb9690701b4913b241634e992ddfe69d3fa
xigolle <joey.driessen@live.nl>
2016-01-05 12:35:02 +0100

    Added request method && module Request
    This method gets a website and logs the body it retrieves to the
    console. Module Requests makes it easy for http requests

--- server.js
+++ server.js
@@ -7,10 +7,15 @@
 
 var express = require("express");
 var app = express();
+var request = require("request");
 var path = require('path');
 
 
-
+request('http://modulus.io', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body)//show the HTML for the modulus homepage
+    }
+});
 app.use(express.static(path.join(__dirname, '')));
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname+"/index.html"));

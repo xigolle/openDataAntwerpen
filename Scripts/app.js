5ae2b704fcc28ac08560ff84314b22776721a616
mathiassamyn <mathiassamyn@hotmail.com>
2016-01-12 19:45:23 +0100

    user location

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,6 +2,8 @@
 
 
 var OpenWifiData; //variable to store openWifiData
+var initialLocation;
+var browserSupportFlag = new Boolean();
 
 //Json controller to get the JSON data from the api
 app.controller("MapController", function ($scope, $interval,$http) {
@@ -33,7 +35,34 @@ app.controller("MapController", function ($scope, $interval,$http) {
                     icon: icon
                     
                 });
-            }          
+            }
+
+            // Try W3C Geolocation (Preferred)
+            if (navigator.geolocation) {
+                browserSupportFlag = true;
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                    map.setCenter(initialLocation);
+                }, function () {
+                    handleNoGeolocation(browserSupportFlag);
+                });
+            }
+                // Browser doesn't support Geolocation
+            else {
+                browserSupportFlag = false;
+                handleNoGeolocation(browserSupportFlag);
+            }
+
+            function handleNoGeolocation(errorFlag) {
+                if (errorFlag == true) {
+                    alert("Geolocation service failed.");
+                    //initialLocation = newyork;
+                } else {
+                    alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
+                    //initialLocation = siberia;
+                }
+                //map.setCenter(initialLocation);
+            }
 
             directionsDisplay.setMap(map);
 
@@ -67,6 +96,8 @@ app.controller("MapController", function ($scope, $interval,$http) {
     });
    
 });
+
+
 
 
 

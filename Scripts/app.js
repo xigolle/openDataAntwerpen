ac74986d93a07c9cc7a019b30484c1395cae47a6
mathiassamyn <mathiassamyn@hotmail.com>
2016-01-13 17:21:07 +0100

    closest marker

--- Scripts/app.js
+++ Scripts/app.js
@@ -8,7 +8,9 @@ var bootstrap = angular.module("myBootstrap", ['ui.bootstrap'])
 
 var OpenWifiData; //variable to store openWifiData
 var initialLocation;
-var browserSupportFlag = new Boolean();
+var geoLocation = new Boolean();
+var distance = 999999999999999999999999999999999999;
+var closest;
 
 app.factory('myService', function ($http) {
     var myService = {
@@ -39,85 +41,92 @@ app.controller("MapController", function ($scope, $interval, $http, myService) {
         initialize();
     });
 
-    
+        
     
         initialize = function () {
             directionsService = new google.maps.DirectionsService;
             directionsDisplay = new google.maps.DirectionsRenderer;
             map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 14,
-                //center: { lat: 51.219710, lng: 4.409398 }
+                zoom: 14
             });
 
             var icon = {
                 url: "../Images/WifiIcon.png",
                 scaledSize: new google.maps.Size(50,50)
-                //origin: new google.maps.Point(0,0)
-            };
-
-            for (var i = 0 ; i < OpenWifiData.data.length; i++) {
-                marker = new google.maps.Marker({
-                    map: map,
-                    position: { lat:  parseFloat(OpenWifiData.data[i].point_lat), lng: parseFloat(OpenWifiData.data[i].point_lng) },
-                    title: OpenWifiData.data[i].locatie,
-                    icon: icon
-
-                });
-            }
+            };         
 
             // Try W3C Geolocation (Preferred)
-            if (navigator.geolocation) {
-                browserSupportFlag = true;
+            if (navigator.geolocation) {               
                 navigator.geolocation.getCurrentPosition(function (position) {
                     initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                    geoLocation = true;
                     map.setCenter(initialLocation);
+                    putMarkers(position.coords.latitude, position.coords.longitude, geoLocation);
+                    
                 }, function () {
-                    handleNoGeolocation(browserSupportFlag);
+                    geoLocation = false;
+                    handleNoGeolocation(geoLocation);
                 });
             }
                 // Browser doesn't support Geolocation
             else {
-                browserSupportFlag = false;
-                handleNoGeolocation(browserSupportFlag);
+                geoLocation = false;
+                handleNoGeolocation(geoLocation);
             }
 
-            function handleNoGeolocation(errorFlag) {
-                if (errorFlag == true) {
-                    alert("Geolocation service failed.");
-                    //initialLocation = newyork;
-                } else {
-                    alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
-                    //initialLocation = siberia;
-                }
+            function handleNoGeolocation(geoLocationSucces) {
                 map.setCenter({ lat: 51.219710, lng: 4.409398 });
+                putMarkers(51.219710, 4.409398, geoLocationSucces);
             }
 
-            marker = new google.maps.Marker({
-                map: map,
-                position: initialLocation,
-                title: "User"
-                
-            });
+            function putMarkers(userLat, userLong, geoLocationSucces) {
 
-            console.log('usermarker');
-            directionsDisplay.setMap(map);
+                for (var i = 0 ; i < OpenWifiData.data.length; i++) {
+                    marker = new google.maps.Marker({
+                        map: map,
+                        position: { lat: parseFloat(OpenWifiData.data[i].point_lat), lng: parseFloat(OpenWifiData.data[i].point_lng) },
+                        title: OpenWifiData.data[i].locatie,
+                        icon: icon
+
+                    });
+                    
+                    newDistance = Math.abs(userLat - parseFloat(OpenWifiData.data[i].point_lat)) + Math.abs(userLong - parseFloat(OpenWifiData.data[i].point_lng));
+                    if (distance > newDistance) {
+                        distance = newDistance;
+                        closest = OpenWifiData.data[i];
+                    }
+                    
+                }
+                if (geoLocationSucces) {
+                    marker = new google.maps.Marker({
+                        map: map,
+                        position: initialLocation,
+                        title: "User"
+                    });
+                    directionsDisplay.setMap(map);
+                    calculateAndDisplayRoute(/*directionsService, directionsDisplay*/);
+                }
+            }
+
+           
+
+            /*directionsDisplay.setMap(map);
 
             onChangeHandler = function () {
                 calculateAndDisplayRoute(directionsService, directionsDisplay);
             };
-            //onChangeHandler();
+            //onChangeHandler();*/
         }
-
+      
 
         calculateAndDisplayRoute = function () {
             directionsService.route({
-                origin: { lat: 40.85, lng: -87.65 },
-                destination: { lat: 45.85, lng: -87.35 },
+                origin: initialLocation,
+                destination: { lat: parseFloat(closest.point_lat), lng: parseFloat(closest.point_lng) },
                 travelMode: google.maps.TravelMode.WALKING
             }, function (response, status) {
                 if (status === google.maps.DirectionsStatus.OK) {
                     directionsDisplay.setDirections(response);
-                    console.log("het werkt volledig");
                 } else {
                     window.alert('Directions request failed due to ' + status);
                 }
